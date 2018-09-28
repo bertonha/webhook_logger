b87b0d9400ff62a4fdcfe921f75b2d96cbfd5d04
Christofer Bertonha <christoferbertonha@gmail.com>
2018-09-28 11:13:05 +0200

    initial version

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, request
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def logger():
+    app.logger.warning('METHOD: %s', request.method)
+    app.logger.warning('HEADERS: %s', request.headers)
+    app.logger.warning('QUERY_PARAMS: %s', request.args)
+    app.logger.warning('POST_DATA: %s', request.form)
+    return 'OK'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
